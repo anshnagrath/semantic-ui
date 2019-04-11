814813442c4107cb2be08f2793195468ee1a936c
anshnagrath <anshnagrath448@gmail.com>
2019-04-11 19:56:27 +0530

    react Learning

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
 import CommentDetails from './CommentDetails';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 const App = ()=>{
 return (
 <div className="ui container comments">
-<CommentDetails/>
+<ApprovalCard>
+<CommentDetails avatar={faker.image.avatar()} content="good one" timeAgo="Today at 4:45PM" author="Sam"/>
+</ApprovalCard>
+<ApprovalCard>
+<CommentDetails avatar={faker.image.avatar()} content="nice" timeAgo="Today at 2:00AM" author="Alex"/>
+</ApprovalCard>
+<ApprovalCard>
+<CommentDetails avatar={faker.image.avatar()} content="Hawt" timeAgo="Yesterday at 5:00PM" author="Jane"/>
+</ApprovalCard>
+
 </div>
 );
 } 
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+
